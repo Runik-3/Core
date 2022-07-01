bf88475de9d0027f4f5974356f85bc79ef569d76
matteomiceli <omatteomiceli@gmail.com>
2022-06-30 21:26:35 -0700

    general setup
    - updated docs
    - added fonts and logo
    - package folder for generator and conversion services

--- src/renderer/src/views/HomeView.vue
+++ src/renderer/src/views/HomeView.vue
@@ -1,8 +1,15 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <main>
-    <p>Taco</p>
-  </main>
+	<main>
+		<p class="font-header">Taco</p>
+		<logo />
+	</main>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Logo from '../components/icons/Logo.vue';
+
+export default defineComponent({
+	components: { Logo },
+});
+</script>
