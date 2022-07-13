52a7be3e3c26bf813af06f47e12b81fd0cb752c9
matteomiceli <omatteomiceli@gmail.com>
2022-07-13 14:59:19 -0700

    basic "filter" on Map object
    - need to consider alternative that is non-destructive and more reminiscent of .filter()

--- src/renderer/src/components/DictionaryEdit.vue
+++ src/renderer/src/components/DictionaryEdit.vue
@@ -1,9 +1,16 @@
 <template>
 	<button @click="closeDict" class="close w-full text-right">X</button>
+	<!-- search component -->
+	<input
+		type="text"
+		v-model="searchDefs"
+		class="border border-gray-400"
+		@input="filterDefs"
+	/>
 	<List>
 		<ListItem
 			:isEditable="true"
-			v-for="[word, definition] in dictionaryData.definitions"
+			v-for="[word, definition] in dictionaryDefinitions"
 			:key="word"
 			:title="word"
 			:secondary="definition"
@@ -19,9 +26,33 @@ import List from '../components/List.vue';
 export default defineComponent({
 	props: ['dictionaryData'],
 
+	data(): {
+		dictionaryDefinitions: Map<string, string>;
+		searchDefs: string;
+	} {
+		return {
+			dictionaryDefinitions: new Map(this.dictionaryData.definitions),
+			searchDefs: '',
+		};
+	},
+
 	methods: {
 		closeDict() {
 			this.$emit('closeDict');
+		},
+
+		filterDefs() {
+			const query = this.searchDefs;
+			if (query.length > 3) {
+				this.dictionaryDefinitions.forEach((def, word) => {
+					if (
+						!def.toLowerCase().includes(query.toLowerCase()) ||
+						!word.toLowerCase().includes(query.toLowerCase())
+					) {
+						this.dictionaryDefinitions.delete(word);
+					}
+				});
+			}
 		},
 	},
 
